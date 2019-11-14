19e426ceda2f99292860c9879016e85675fe00cd
almathaler <athaler00@stuy.edu>
2019-11-13 20:33:40 -0500

    the permissions is wrong way around

--- stat_stuff.c
+++ stat_stuff.c
@@ -26,8 +26,25 @@ void print_file_size(char *path, struct stat *StaT){
 void print_mode(char *path, struct stat *StaT){
   stat(path, StaT);
   int mode = StaT->st_mode;
-  mode = mode%01000;
-  printf("mode of the file: %o\n", mode);
+  int octalMode = mode%01000;
+  mode = octalMode;
+  printf("rwx mode: ");
+  int i;
+  int k;
+  int digit;
+  char arr[3] = {'x', 'w', 'r'};
+  for (k=0; k<3; k++){
+    for (i=0; i<3; i++){ //x, then w, then r
+      digit = mode%0b10; //mod by 2 to cut off the last digit
+      mode = mode / 0b10; //divide by 2 to continue on
+      if (digit&0b1){ // one mod one is zero. so if it's on, print
+        printf("%c", arr[i]);
+      }else{
+        printf("-");
+      }
+    }
+  }
+  printf("\noctal mode of the file: %o\n", octalMode);
 }
 void print_atime(char *path, struct stat *StaT){
   stat(path, StaT);
